c71931b42b75de82962c07a7525e52953eff9106
stefaxel <stefan.axelson@sky.com>
2023-04-24 18:47:15 +0100

    Initial commit

---
+++ Source/MyProject/Bullet.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bullet.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+ABullet::ABullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BulletMesh = CreateDefaultSubobject<UStaticMeshComponent>("BulletMesh");
+	SetRootComponent(BulletMesh);
+
+	BulletMovement = CreateDefaultSubobject<UMovementComponent>("BulletMovement");
+
+}
+
+// Called when the game starts or when spawned
+void ABullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
